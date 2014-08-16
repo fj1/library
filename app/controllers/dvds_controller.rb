ec20e4a18f317faf6ae8adb0e32652ce38c92015
Frances Jurek <jurek.frances@gmail.com>
2014-08-16 18:56:04 -0500

    added edit/delete/update functionality to dvds

--- app/controllers/dvds_controller.rb
+++ app/controllers/dvds_controller.rb
@@ -1,5 +1,4 @@
 class DvdsController < ApplicationController
-
   def index
     @dvds = Dvd.all
     respond_to do |format|
@@ -21,8 +20,8 @@ class DvdsController < ApplicationController
     actor = Actor.new(actor_params)
     dvd.save!
     actor.save!
-    redirect_to root_path
     ActorDvd.create!( {dvd_id: dvd.id, actor_id: actor.id} )
+    redirect_to '/dvds'
   end
 
   def edit
@@ -30,9 +29,15 @@ class DvdsController < ApplicationController
   end
 
   def update
-    @dvd = Dvd.find(dvd_params[:id])
-    @dvd.update(dvd_params)
+    @dvd = Dvd.find(params[:id])
+    @dvd.update(params.require(:dvd).permit(:id))
     redirect_to '/dvds'
+  end
+
+  def destroy
+    @dvd = Dvd.find(params[:id])
+    @dvd.destroy
+    render status: 200, json: { status: 200 }
   end
 
   # strong params
